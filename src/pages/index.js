3fc6228d06554b081663efcf3c61a4ac6993cc5f
ilias <essatenilas@gmail.com>
2023-05-07 00:00:56 +0300

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,117 +1,119 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { Configuration, OpenAIApi } from 'openai'
+import { useState } from 'react'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  /* */
+  const [prompt, setPrompt] = useState("");
+  const [resposes, setResponses] = useState([]);
+  const [messages, setMessages] = useState ([])
+  const [loading, setLoading] = useState(false); 
+
+
+  const sendMessage = async () => {
+    /*
+    const configuration = new Configuration({
+    apiKey: "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",
+});
+    const openai = new OpenAIApi(configuration)
+
+    setLoading(true)
+
+    // сохранение 
+    setResponses(prevResponses => [...prevResponses, prompt])
+
+    const respose = await openai.createCompletion({
+      
+      model:"text-davinci-003", // генерирует простые текста гпт 3,0
+      prompt: prompt, // сообщение которые отсылаю модели
+      temperature: 0.7, // отвечает за креативность
+      max_tokens: 2048, // максимальное к-во токенов которое потрачу при запросе 
+    });
+
+    setPrompt("")
+
+    setLoading(false)
+
+    // заносит ответ от бота в саму модель  response.data.choices[0].text ответ от модели в результате
+    setResponses(prevResponses => [...prevResponses, respose.data.choices[0].text])*/
+
+    //
+
+
+    messages.push({role:"user", content: prompt}) // добавляет в переменную мои сообщения
+
+    let options = {
+      method:"POST",
+      headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 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'
+      },
+      body:JSON.stringify({
+        'model': 'gpt-3.5-turbo',
+        'messages':messages
+      })
+    }
+
+    
+    setLoading(true)
+
+
+    // сохранение  https://www.youtube.com/watch?v=uFOwaRjh1EE
+    setResponses(prevResponses => [...prevResponses, prompt])
+
+
+    setPrompt("")
+
+    const response = await fetch ('https://api.openai.com/v1/chat/completions', options)
+    const data = await response.json()
+
+    setLoading(false)
+
+
+    setResponses(prevResponses => [...prevResponses, data.choices[0].message.content])
+    setMessages(prevResponses => [...prevResponses, {role:"assistant", content: data.choices[0].message.content},{role:"system",
+    content: "You're a JavaScipt Mentor, so talk like JavaScript Developer"}])
+    messages.push({role:"assistant", content: data.choices[0].message.content},{role:"system",
+    content: "You're a JavaScipt Mentor, so talk like JavaScript Developer my main goal is to get a job as a front-end developer, and your task is to help me in this now i know HTML CSS JavaScript i want you to bring me closer to my goal to get job as Frond end developer of getting a job in every post you are my mentor Frontend Developer"}) // добавляет в переменную ответы чата
+
+  } 
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className='container flex justify-center w-full mx-auto bg-black'>
+        <div className='w-1/3 mt-20'>
+        <h2 className='mb-5 text-5xl text-center text-white'>MY CHAT APP</h2>
+        <div className='border rounded-md '>
+
+          <div className='p-6 border-b'> 
+          <p className='text-center text-white text-md'>Your chat history</p>
+          </div>
+          <div className='p-6 border-b'>
+
+            {resposes && resposes.map((item, index) => {
+              return(
+                <div  key={index} className='p-4 rounded-md bg-zinc-500'>
+                    <p className='mr-2 text-xs text-zinc-200'>[{index + 1}]</p>
+                    <p className='text-sm text-white'>{item}</p>
+                  </div>
+              )
+            })}
+              {loading && (
+                <p className='text-white'>loading...</p>
+              )}
+          </div>
+          <div className='flex p-6 border-t '>
+            <input onChange={(e) => {setPrompt(e.target.value)}} value ={prompt} type ="text" className='w-8/12 px-4 py-2 text-white bg-zinc-600 roundend-tl-md rounded-td-md' placeholder='Your message goes here'/>
+            <button onClick={() => {sendMessage()}} type = "button" className='w-4/12 text-white bg-zinc-700 rounded-tr-md rounded-br-md'>Send</button>
+          </div>
+
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
       </div>
     </main>
   )
